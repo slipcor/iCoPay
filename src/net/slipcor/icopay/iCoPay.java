b291d371f445940f293466fdacad61180d9ef3d9
Chris Fehling <chris@slipcor.de>
2012-05-22 13:08:14 +0200

    v0.0.2.0 - iCo6 support

--- src/net/slipcor/icopay/iCoPay.java
+++ src/net/slipcor/icopay/iCoPay.java
@@ -1,34 +1,26 @@
 package net.slipcor.icopay;
 
-import java.util.logging.Logger;
-
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.iCo6.system.Accounts;
 import com.iConomy.iConomy;
 
-/*
+/**
  * main class
  * 
- * author: slipcor
+ * @author slipcor
  * 
- * version: v0.0.0 - basic functionality
- * 
- * history:
- * 
- *     v0.0.0 - basic functionality
+ * @version: v0.0.2
  * 
  */
 
 public class iCoPay extends JavaPlugin {
-	private final Logger log = Logger.getLogger("Minecraft");
-	private int SPAWN_ID = -1;
+	private int pay_id = -1;
 
 	@Override
 	public void onEnable() {
-		log.info("[iCoPay] " + getDescription().getVersion() + " enabled");
 		getConfig().addDefault("interval", 15);
 		getConfig().addDefault("amount", 25.00d);
 		getConfig().options().copyDefaults(true);
@@ -37,36 +29,41 @@ public class iCoPay extends JavaPlugin {
 		int interval = getConfig().getInt("interval", 0) * 20 * 60;
 
 		if (interval > 0) {
-			SPAWN_ID = Bukkit
+			pay_id = Bukkit
 					.getServer()
 					.getScheduler()
-					.scheduleSyncRepeatingTask(this, autoSave, interval,
+					.scheduleSyncRepeatingTask(this, autoPay, interval,
 							interval);
 		}
-
+		Bukkit.getLogger().info(getDescription().getFullName() + " enabled");
 	}
 
 	@Override
 	public void onDisable() {
-		Bukkit.getScheduler().cancelTask(SPAWN_ID);
-		log.info("[iCoPay] " + getDescription().getVersion() + " disabled");
+		Bukkit.getScheduler().cancelTask(pay_id);
+		Bukkit.getLogger().info(getDescription().getFullName() + " disabled");
 	}
 
-	private Runnable autoSave = new Runnable() {
+	private Runnable autoPay = new Runnable() {
 		public void run() {
 			Player[] players = Bukkit.getOnlinePlayers();
 			double d = getConfig().getDouble("amount", 25.00d);
 			for (Player p : players) {
 				try {
 					iConomy.Accounts.get(p.getName()).getHoldings().add(d);
-					p.sendMessage(ChatColor.DARK_GREEN + "[" + ChatColor.WHITE
-							+ "Money" + ChatColor.DARK_GREEN + "] "
-							+ ChatColor.DARK_GREEN + "Your account had "
-							+ ChatColor.WHITE + iConomy.format(d)
-							+ ChatColor.DARK_GREEN + " credited.");
+					p.sendMessage("§2[§fMoney§2] Your account had §f"
+							+ iConomy.format(d) + " §2credited.");
 				} catch (Exception e) {
 					// I don't care
 				}
+				try {
+					(new Accounts()).get(p.getName()).getHoldings().add(d);
+					p.sendMessage("§2[§fMoney§2] Your account had §f"
+							+ com.iCo6.iConomy.format(d) + " §2credited.");
+				} catch (Exception e) {
+					// I don't care
+				}
+				
 			}
 		}
 	};
